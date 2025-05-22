9f56bca02947dddabc8c24bedae3c5bfdcf28fd8
timmy <yangtmeco@outlook.com>
2025-05-22 22:36:09 +0800

    config: enable formatters and disable astro in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
-  formatters: false,
-  astro: true,
+  formatters: true,
+  // astro: true,
   stylistic: {
     quotes: "double",
     semi: true,
